b98db439bf02ca843b8682fe392486aab8690847
Nicholas Vilela <nvilela01@gmail.com>
2018-12-25 21:20:50 -0500

    added template routing and dashboard

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Login = () => <h2>Login</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const DashboardProto = () => <h2>DashboardProto</h2>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div>
+            <Route exact path="/"         component={Login} />
+            <Route path="/dashboard"      component={Dashboard} />
+            <Route path="dashboard/proto" component={DashboardProto} />
+            {/* 
+              <Route exact path="/" component={} />
+            */}
+          </div>
+        </div>
+      </Router>
     );
   }
 }
